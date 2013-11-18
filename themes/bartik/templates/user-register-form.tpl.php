fa794a471a8faf7079acd6fc38507d39a207d499
sharmila.g <sharmila.g@edcellence.in>
2013-11-18 17:21:55 +0530

    added signup form

---
+++ themes/bartik/templates/user-register-form.tpl.php
@@ -0,0 +1,6 @@
+<p><?php print render($intro_text); ?></p>
+
+<?php print render($form['account']); ?>
+<?php print render($form['form_build_id']); ?>
+<?php print render($form['form_id']); ?>
+<?php print drupal_render($form['actions']); ?>
