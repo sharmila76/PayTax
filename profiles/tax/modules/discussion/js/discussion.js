56e0c803aa7d87ff6c4f1550beabf71a1e6b1973
thaiyalnayaki <thaiyal.n@edcellence.in>
2013-11-21 10:39:48 +0530

    Modified router page

--- profiles/tax/modules/discussion/js/discussion.js
+++
@@ -1,9 +0,0 @@
-(function ($) { 
- Drupal.behaviors.discussion = {
-    attach: function (context, settings) {
-  $('.discussion_form_submit').click(function(){
-  $('.selected').hide();
- });
- }
- };
- })(jQuery);
