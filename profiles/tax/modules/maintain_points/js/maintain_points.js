1efc3deebe1780b1493772d28a84e4411f4bbf5d
sharmila.g <sharmila.g@edcellence.in>
2013-11-28 11:42:35 +0530

    displayed a points when the user selects the category

--- profiles/tax/modules/maintain_points/js/maintain_points.js
+++ profiles/tax/modules/maintain_points/js/maintain_points.js
@@ -1,9 +1,15 @@
-(function ($) {
+(function($) {
   Drupal.behaviors.maintain_points = {
     attach: function(context, settings) {
       $('#edit-field-select-category').click(function() {
-        var category = $('#edit-field-select-category :selected').text();        
-        $('.description').text('hai ' + category);
+        var category = $('#edit-field-select-category :selected').text();
+        if (category != "- Select -") {
+          var cat_split = category.split('-');
+          $('.form-item-field-select-category div:last').text('for this category ' + cat_split[1] + ' points will be reduced.');
+        }
+        else {
+          $('.form-item-field-select-category div:last').text('Select Category');
+        }
       });
     }
   };
