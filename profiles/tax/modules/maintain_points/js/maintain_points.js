d7234f794b7c0336673edba98caa22b16f5e9783
sharmila.g <sharmila.g@edcellence.in>
2013-11-29 13:16:16 +0530

    added maintain points js file

--- profiles/tax/modules/maintain_points/js/maintain_points.js
+++ profiles/tax/modules/maintain_points/js/maintain_points.js
@@ -2,14 +2,19 @@
   Drupal.behaviors.maintain_points = {
     attach: function(context, settings) {
       $('#edit-field-select-category').click(function() {
-        var category = $('#edit-field-select-category :selected').text();
-        if (category != "- Select -") {
-          var cat_split = category.split('-');
-          $('.form-item-field-select-category div:last').text('for this category ' + cat_split[1] + ' points will be reduced.');
-        }
-        else {
-          $('.form-item-field-select-category div:last').text('Select Category');
-        }
+        var selected_category = $('#edit-field-select-category :selected').text();
+        var category_dict = Drupal.settings.maintain_points.categories_list;
+        var parsedData = JSON.parse(category_dict);
+        $.each(parsedData, function(key, value) {
+          if (selected_category != "- Select -") {
+            if (value == selected_category) {
+              $('.form-item-field-select-category div:last').text('for this category ' + key + ' points will be reduced.');
+            }
+          }
+          else {
+            $('.form-item-field-select-category div:last').text('Select Category');
+          }
+        });
       });
     }
   };
