beaf3945ee5a26a8a19621a2f214601133fdb9a3
sharmila.g <sharmila.g@edcellence.in>
2013-11-29 20:09:04 +0530

    added select category field and displayed the points based on the selected category

--- profiles/tax/modules/maintain_points/js/maintain_points.js
+++ profiles/tax/modules/maintain_points/js/maintain_points.js
@@ -1,18 +1,19 @@
 (function($) {
   Drupal.behaviors.maintain_points = {
     attach: function(context, settings) {
-      $('#edit-field-select-category').click(function() {
-        var selected_category = $('#edit-field-select-category :selected').text();
-        var category_dict = Drupal.settings.maintain_points.categories_list;
+      $('#edit-field-select-category-und').click(function() {
+        var selected_category = $('#edit-field-select-category-und :selected').text();
+        var category_dict = Drupal.settings.maintain_points.categories_list;        
         var parsedData = JSON.parse(category_dict);
         $.each(parsedData, function(key, value) {
-          if (selected_category != "- Select -") {
+          if (selected_category != "- Select a value -") {
             if (value == selected_category) {
-              $('.form-item-field-select-category div:last').text('for this category ' + key + ' points will be reduced.');
+              var points = key.split('-'); 
+              $('.form-item-field-select-category-und div:last').text('for this category ' + points[1] + ' points will be reduced.');
             }
           }
           else {
-            $('.form-item-field-select-category div:last').text('Select Category');
+            $('.form-item-field-select-category-und div:last').text('Category for the question');
           }
         });
       });
