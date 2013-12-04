5a24f35f672ebc93f3ad9d70751da83b96d77557
sharmila.g <sharmila.g@edcellence.in>
2013-12-04 14:41:45 +0530

    added drag and drop file upload option in the post question form

--- profiles/tax/modules/maintain_points/js/maintain_points.js
+++ profiles/tax/modules/maintain_points/js/maintain_points.js
@@ -1,19 +1,19 @@
 (function($) {
   Drupal.behaviors.maintain_points = {
     attach: function(context, settings) {
-      $('#edit-field-select-category-und').click(function() {
-        var selected_category = $('#edit-field-select-category-und :selected').text();
+      $('#edit-field-select-category-name').click(function() {
+        var selected_category = $('#edit-field-select-category-name :selected').text();
         var category_dict = Drupal.settings.maintain_points.categories_list;        
         var parsedData = JSON.parse(category_dict);
         $.each(parsedData, function(key, value) {
-          if (selected_category != "- Select a value -") {
+          if (selected_category != "- Select -") {
             if (value == selected_category) {
               var points = key.split('-'); 
-              $('.form-item-field-select-category-und div:last').text('for this category ' + points[1] + ' points will be reduced.');
+              $('.form-item-field-select-category-name div:last').text('for this category ' + points[1] + ' points will be reduced.');
             }
           }
           else {
-            $('.form-item-field-select-category-und div:last').text('Category for the question');
+            $('.form-item-field-select-category-name div:last').text('Category for the question');
           }
         });
       });
